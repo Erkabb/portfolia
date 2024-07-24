5c7660701ad490553df93fe57beb18d812911d5e
erkabb <erkabb816@gmail.com>
2024-07-24 12:41:23 +0800

    d

--- src/components/main/skills.jsx
+++ src/components/main/skills.jsx
@@ -4,17 +4,29 @@ import { FaNodeJs } from "react-icons/fa";
 import { RiNextjsLine } from "react-icons/ri";
 import { FaGitSquare } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress } from "react-icons/si";
+import { SiNestjs } from "react-icons/si";
+import { SiSocketdotio } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiMongodb } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+import { IoLogoFigma } from "react-icons/io5";
+import { TbBrandCypress } from "react-icons/tb";
+import { TbBrandStorybook } from "react-icons/tb";
+import { SiTypescript } from "react-icons/si";
 
 const Skills = () => {
   return (
     <section className="flex flex-col items-center gap-16 mt-20">
-      <div className="h-20 w-full flex flex-col justify-center items-center gap-7">
+      <div className="h-20 w-full flex flex-col justify-center items-center gap-7 ">
         <button className="h-7 w-20 bg-gray-200 rounded-2xl text-black ">
           Skills
         </button>
-        <span>The skills, tools and technologies I am really good at:</span>
+        <span className="text-center">
+          The skills, tools and technologies I am really good at:
+        </span>
       </div>
-      <div className="flex flex-row gap-5">
+      <div className="w-[1100px] h-[370px] flex flex-row flex-wrap gap-16 text-center">
         <div>
           <IoLogoJavascript size={64} />
           <span>Javascript</span>
@@ -38,6 +50,46 @@ const Skills = () => {
         <div>
           <RiTailwindCssFill size={64} />
           <span>Tailwind.css</span>
+        </div>
+        <div>
+          <SiExpress size={64} />
+          <span>Express.js</span>
+        </div>
+        <div>
+          <SiNestjs size={64} />
+          <span>Nest.js</span>
+        </div>
+        <div>
+          <SiSocketdotio size={64} />
+          <span>Socket.io</span>
+        </div>
+        <div>
+          <BiLogoPostgresql size={64} />
+          <span>PostgreSQL</span>
+        </div>
+        <div>
+          <SiMongodb size={64} />
+          <span>Mongodb</span>
+        </div>
+        <div>
+          <FaSass size={64} />
+          <span>Sass/Scss</span>
+        </div>
+        <div>
+          <IoLogoFigma size={64} />
+          <span>Figma</span>
+        </div>
+        <div>
+          <TbBrandCypress size={64} />
+          <span>Cypress</span>
+        </div>
+        <div>
+          <TbBrandStorybook size={64} />
+          <span>Storybook</span>
+        </div>
+        <div>
+          <SiTypescript size={64} />
+          <span>Typescript</span>
         </div>
       </div>
     </section>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,20 @@
 import Aboutme from "@/components/main/aboutme";
+import Experience from "@/components/main/experience";
 import Introduction from "@/components/main/introduction";
 import Pfp from "@/components/main/pfp";
 import Skills from "@/components/main/skills";
 
 export default function Home() {
   return (
-    <main>
-      <section className="flex flex-col gap-20">
-        <div className="flex justify-center items-center gap-20 ">
-          <Introduction />
-          <Pfp />
-        </div>
+    <main className="flex flex-col gap-20">
+      <div className="flex justify-center items-center gap-20 ">
+        <Introduction />
+        <Pfp />
+      </div>
 
-        <Aboutme />
-        <Skills />
-      </section>
+      <Aboutme />
+      <Skills />
+      <Experience />
     </main>
   );
 }
